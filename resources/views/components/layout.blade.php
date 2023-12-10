adf11e160df04ceef714ffda3c3676f6cb4b7d1f
Diego Vogel <diego@birdboar.co>
2023-12-09 23:35:47 -0500

    Details and Summary demo done.

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0"/>
+
+    <title>{{ $title ?? 'State of HTML Demos' }}</title>
+
+    <link rel="stylesheet"
+          href="https://unpkg.com/normalize.css@8.0.1/normalize.css"/>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <meta http-equiv="X-UA-Compatible"
+          content="IE=edge"/>
+
+    <meta charset="UTF-8"/>
+</head>
+
+<body>
+<main>
+    {{ $slot }}
+</main>
+</body>
+
+</html>
---
+++ resources/views/demo-b.blade.php
@@ -0,0 +1,46 @@
+<x-layout>
+    <h1>Demo B</h1>
+
+    <h2><code>&lt;details&gt;</code> and <code>&lt;summary&gt;</code></h2>
+
+    <details>
+        <summary>Basic Example</summary>
+
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam
+        malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.
+
+        <ul>
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+            <li>Aliquam tincidunt mauris eu risus.</li>
+            <li>Vestibulum auctor dapibus neque.</li>
+            <li>Nunc dignissim risus id metus.</li>
+            <li>Cras ornare tristique elit.</li>
+            <li>Vivamus vestibulum ntulla nec ante.</li>
+            <li>Praesent placerat risus quis eros.</li>
+            <li>Fusce pellentesque suscipit nibh.</li>
+            <li>Integer vitae libero ac risus egestas placerat.</li>
+        </ul>
+
+        <img src="https://images.unsplash.com/photo-1546853020-ca4909aef454?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+             alt="">
+    </details>
+
+    <details class="styled">
+        <summary>Styled Example</summary>
+
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec
+        urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet
+        viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse
+        potenti.
+    </details>
+
+    <p>
+        <code>&lt;details&gt;</code> can also be animated with the Web Animations API, though it is non-trivial.
+    </p>
+
+{{--  TODO: highly-styled collapsible page section.  --}}
+
+    <h2><code>&lt;dialog&gt;</code></h2>
+
+    <h2><code>&lt;popover&gt;</code></h2>
+</x-layout>
