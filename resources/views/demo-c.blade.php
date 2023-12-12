887a3c2a92bfb732f10aa36c0a531e135bacf5ef
Diego Vogel <diego@birdboar.co>
2023-12-12 11:38:21 -0500

    Demo C done

--- resources/views/demo-c.blade.php
+++ resources/views/demo-c.blade.php
@@ -7,4 +7,70 @@
 
 <x-layout>
     <h1>Demo C: Web Components</h1>
+
+    <p>Benefit: framework agnostic. Use <strong>the same components</strong> across multiple websites.</p>
+
+    <p>Features include:</p>
+
+    <ul>
+        <li>Slots</li>
+        <li>Properties</li>
+        <li>Parts</li>
+    </ul>
+
+    <hr>
+
+    <div class="shoelace-demos">
+        <sl-button>
+            Button
+        </sl-button>
+
+        <sl-progress-bar value="0"></sl-progress-bar>
+
+        <sl-image-comparer>
+            <img
+                slot="before"
+                src="https://images.unsplash.com/photo-1517331156700-3c241d2b4d83?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80&sat=-100&bri=-5"
+                alt="Grayscale version of kittens in a basket looking around."
+            />
+            <img
+                slot="after"
+                src="https://images.unsplash.com/photo-1517331156700-3c241d2b4d83?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
+                alt="Color version of kittens in a basket looking around."
+            />
+        </sl-image-comparer>
+    </div>
+
+    <style>
+        .shoelace-demos {
+            display: flex;
+            flex-direction: column;
+            gap: 4em;
+            align-items: flex-start;
+            padding: 3em 0;
+        }
+
+        sl-progress-bar {
+            width: 100%;
+        }
+    </style>
+
+    <script>
+        const progressBar = document.querySelector('sl-progress-bar');
+
+        updateProgress();
+
+        function updateProgress() {
+            let value = parseInt(progressBar.getAttribute('value'));
+
+            if (value >= 100) {
+                value = -20;
+            }
+
+            setTimeout(() => {
+                progressBar.setAttribute('value', value + 20);
+                updateProgress();
+            }, 2000);
+        }
+    </script>
 </x-layout>
