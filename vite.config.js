394f60652a78274ffb649113a71c6e686cf8a015
Diego Vogel <diego@birdboar.co>
2023-12-13 16:08:27 -0500

    Add missing entry points for Vite build script.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/scss/app.scss', 'resources/js/app.js'],
+            input: [
+                'resources/scss/app.scss',
+                'resources/scss/demo-b.scss',
+                'resources/scss/sandbox.scss',
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
     ],
